081f2ac5f5252fb6f9af2eb79f2e321d8c42e9d2
Tadeáš Horák <xhorakt00@vutbr.cz>
2025-08-26 03:19:04 +0200

    uart protocol first implementation

--- BPpicoFW.c
+++ BPpicoFW.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 #include "hardware/irq.h"
@@ -21,47 +22,94 @@
 
 // Stepper driver enable
 #define EN_SENSE_PIN 12
-#define EN_PIN 13
+#define EN_PIN 13   // a4988 drivers (and compatibles) use LOW to enable
 
 #define TEMP_SENSE_PIN 15
 #define FAN_PWM_PIN 14
+#define ONBOARD_LED_PIN 25
 
 #define CMD_BUFFER_SIZE 128
 char cmd_buffer[CMD_BUFFER_SIZE];
 int cmd_buffer_index = 0;
-
+enum Commands {
+    CMD_OK = 0x01,
+    CMD_PAUSE = 0x02,
+    CMD_RESUME = 0x03,
+    CMD_STOP = 0x04
+};
+bool is_paused = false;
 void on_uart_rx()
 {
+    // Process incoming data
+    // First byte is START byte 0xAA
+    // Second byte is command type (defined in enum Commands)
+    // Third byte is data length
+    // DATA bytes follow
+    // Last byte is checksum
     while (uart_is_readable(UART_ID)) {
-        char c = uart_getc(UART_ID);
-        if (c == '\n' || c == '\r') {
-            cmd_buffer[cmd_buffer_index] = '\0';
-            printf("Received command: %s\n", cmd_buffer);
+        uint8_t c = uart_getc(UART_ID);
+        if (cmd_buffer_index >= CMD_BUFFER_SIZE - 1) {
+            fprintf(stderr, "ERROR: Command buffer overflow!\n");
             cmd_buffer_index = 0;
-        } 
-        else {
-            if (cmd_buffer_index < CMD_BUFFER_SIZE - 1) {
-                cmd_buffer[cmd_buffer_index++] = c;
-            }
-            else {
-                fprintf(stderr, "ERROR: Command buffer overflow!\n");
-                cmd_buffer_index = 0;
+            continue;
+        }
+        if (cmd_buffer_index == 0 && c != (char)0xAA) {
+            continue;
+        }
+        cmd_buffer[cmd_buffer_index++] = c;
+        if (cmd_buffer_index >= 3) {
+            int data_length = (unsigned char)cmd_buffer[2];
+            if (cmd_buffer_index == data_length + 4) { // DATA + START + CMD + LEN + CHK
+                // Full command received
+                // Verify checksum
+                char checksum = 0;
+                for (int i = 0; i < cmd_buffer_index - 1; i++) {
+                    checksum ^= cmd_buffer[i];
+                }
+                if (checksum != cmd_buffer[cmd_buffer_index - 1]) {
+                    fprintf(stderr, "ERROR: Checksum mismatch!\n");
+                    // Try to resync by looking for next 0xAA
+                    int i = 1;
+                    while (i < cmd_buffer_index && cmd_buffer[i] != 0xAA) i++;
+                    if (i < cmd_buffer_index) {
+                        memmove(cmd_buffer, &cmd_buffer[i], cmd_buffer_index - i);
+                        cmd_buffer_index -= i;
+                    } else {
+                        cmd_buffer_index = 0;
+                    }
+                    continue;
+                }
+                // Process fast commands here, movements will be handled in main loop to avoid blocking
+                switch (cmd_buffer[1]) {
+                    case CMD_OK:
+                        printf("Received CMD_OK\n");
+                        break;
+                    case CMD_PAUSE:
+                        printf("Received CMD_PAUSE\n");
+                        is_paused = true;
+                        break;
+                    case CMD_RESUME:
+                        printf("Received CMD_RESUME\n");
+                        is_paused = false;
+                        break;
+                    case CMD_STOP:
+                        printf("Received CMD_STOP\n");
+                        gpio_put(EN_PIN, 1); // Disable steppers
+                        break;
+                    default:
+                        fprintf(stderr, "ERROR: Unknown command!\n");
+                        break;
+                }
+                cmd_buffer_index = 0; // Reset buffer index for next command
             }
         }
+
     }
 }
 
 int main()
 {
     stdio_init_all();
-
-    // UART setup
-    uart_init(UART_ID, BAUD_RATE);
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    irq_set_exclusive_handler(UART0_IRQ, on_uart_rx);
-    irq_set_enabled(UART0_IRQ, true);
-    uart_set_irq_enables(UART_ID, true, false);
 
     // GPIO setup
     gpio_init(Y_STEP_PIN); gpio_set_dir(Y_STEP_PIN, GPIO_OUT);
@@ -75,8 +123,20 @@ int main()
     gpio_init(EN_PIN); gpio_set_dir(EN_PIN, GPIO_OUT);
     gpio_init(TEMP_SENSE_PIN); gpio_set_dir(TEMP_SENSE_PIN, GPIO_IN);
     gpio_init(FAN_PWM_PIN); gpio_set_dir(FAN_PWM_PIN, GPIO_OUT);
+    gpio_init(ONBOARD_LED_PIN); gpio_set_dir(ONBOARD_LED_PIN, GPIO_OUT);
 
-    while (true) {
+    // UART setup
+    uart_init(UART_ID, BAUD_RATE);
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    irq_set_exclusive_handler(UART0_IRQ, on_uart_rx);
+    irq_set_enabled(UART0_IRQ, true);
+    uart_set_irq_enables(UART_ID, true, false);
+
+    gpio_put(ONBOARD_LED_PIN, 1); // Turn on onboard LED to indicate ready
+    gpio_put(EN_PIN, 0);
+
+    while (!is_paused) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
